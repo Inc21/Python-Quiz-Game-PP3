73469ff331af4ed292f3dd166c09182f94440070
Inc21 <intc21@gmail.com>
2023-04-06 20:48:24 +0000

    Add correct creds.json and add main game function

--- run.py
+++ run.py
@@ -11,3 +11,15 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('phyton_quiz_leaderboard')
+
+
+def main():
+    """
+    Run all program functions.
+    """
+    print("Works")
+
+
+print("Welcome to Python Quiz Game \n")
+
+
