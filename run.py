60c80c18628f2e0b4bc5191ed5c2679e5b08d0b6
Inc21 <intc21@gmail.com>
2023-04-11 01:17:00 +0100

    Add play again option after game has ended

--- run.py
+++ run.py
@@ -34,6 +34,17 @@ QUESTIONS =  {
 }
 
 
+def game_over():
+    game_over_user = input("Would you like to play again? Type Y for yes or Q to quit: ").lower()
+    if game_over_user == "q":
+        exit()
+    elif game_over_user == "y":
+        run_game()
+    else:
+        print("\nNot a valid option, please enter Y or Q\n")
+        game_over()
+                       
+
 def run_game():
     """
     Run the main Quiz
@@ -45,16 +56,16 @@ def run_game():
         print(f"\nQuestion {num}:")
         print(f"{question}")
         correct_answer = alternatives[0]
-        labled_alternatives = dict(zip(ascii_lowercase, sorted(alternatives)))
-        for label, alternative in labled_alternatives.items():
+        labeled_alternatives = dict(zip(ascii_lowercase, sorted(alternatives)))
+        for label, alternative in labeled_alternatives.items():
             print(f" {label}) {alternative}")
 
-        while (answer_label := input("\nChoice? ").lower()) not in labled_alternatives:
+        while (answer_label := input("\nChoice? ").lower()) not in labeled_alternatives:
             if answer_label == "q":
                 exit()
-            print(f"Not a valid option, please enter {','.join(labled_alternatives)} or q to quit")
+            print(f"Not a valid option, please enter {','.join(labeled_alternatives)} or q to quit")
 
-        answer = labled_alternatives[answer_label]
+        answer = labeled_alternatives[answer_label]
         if answer == correct_answer:
             print("\n Correct! \n")
             points += 10
@@ -63,7 +74,7 @@ def run_game():
             print( f"The answer is {correct_answer!r}, not {answer!r}\n")
             print("Game Over")
             print(f"\nYou scored {points} points by answering {num_correct} questions correctly.\n")
-            exit()
+            game_over()
 
 
 def main():
